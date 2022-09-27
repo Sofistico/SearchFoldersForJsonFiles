837e3509c9189b1c0ab10964cceb92dbf0a98cc1
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2022-09-27 11:09:17 -0300

    Adicionar arquivos de projeto.

---
+++ SearchFoldersForJsonFiles/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SearchFoldersForJsonFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOfResearches = SourceTreeHelper.GetSourceTree<object>(@".\Research\research_*.json");
+            var listOfJsons = SourceTreeHelper.GetSourceTree<object>(@".\Research\*.json");
+            var listOfEverything = SourceTreeHelper.GetSourceTreeWithSubfolders<object>(@".\Research\*", ".json");
+
+            Console.WriteLine($"List of res : {listOfResearches}");
+            Console.WriteLine($"List of jsons : {listOfJsons}");
+            Console.WriteLine($"List of every : {listOfEverything}");
+        }
+    }
+}
---
+++ SearchFoldersForJsonFiles/SourceTreeHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchFoldersForJsonFiles
+{
+    public static class SourceTreeHelper
+    {
+        public static IReadOnlyList<T> GetSourceTree<T>(string wildCard)
+        {
+            string[] files = FileUtils.GetFiles(wildCard);
+
+            List<List<T>> listTList = new();
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                listTList.Add(JsonUtils.JsonDeseralize<List<T>>(files[i]));
+            }
+            List<T> allTList = new();
+
+            foreach (List<T> tList in listTList)
+            {
+                foreach (T t in tList)
+                {
+                    allTList.Add(t);
+                }
+            }
+
+            IReadOnlyList<T> readOnlyList = allTList.AsReadOnly();
+
+            return readOnlyList;
+        }
+
+        public static IReadOnlyList<T> GetSourceTreeWithSubfolders<T>(string wildCard, string extensionToSearch)
+        {
+            string[] files = FileUtils.GetFiles(wildCard);
+
+            List<List<T>> listTList = new();
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                listTList.Add(JsonUtils.JsonDeseralize<List<T>>(files[i]));
+            }
+            List<T> allTList = new();
+
+            foreach (List<T> tList in listTList)
+            {
+                foreach (T t in tList)
+                {
+                    allTList.Add(t);
+                }
+            }
+
+            IReadOnlyList<T> readOnlyList = allTList.AsReadOnly();
+
+            return readOnlyList;
+        }
+    }
+}
